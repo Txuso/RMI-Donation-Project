9b670e43d88ef9cf6ed1c992a68af7f36937f2cf
Txuso <josurubiollona@gmail.com>
2016-01-16 13:41:02 +0100

    - Commit

--- 4.4.2_DonationObserver_Server/src/es/deusto/ingenieria/sd/donation/server/remote/ICollector.java
+++
@@ -1,9 +0,0 @@
-package es.deusto.ingenieria.sd.donation.server.remote;
-
-import java.rmi.RemoteException;
-
-import es.deusto.ingenieria.sd.util.observer.remote.IRemoteObservable;
-
-public interface ICollector extends IRemoteObservable {
-	void getDonation(int donation) throws RemoteException;
-}
--- ObserverUtils/src/es/deusto/ingenieria/sd/util/observer/remote/IRemoteObserver.java
+++
@@ -1,8 +0,0 @@
-package es.deusto.ingenieria.sd.util.observer.remote;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface IRemoteObserver extends Remote {
-	public void update(Object arg) throws RemoteException;
-}
